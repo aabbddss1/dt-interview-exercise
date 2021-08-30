e2c4e2f72165bed3c1c296b4a1ae86288a1c3d22
Samet <74731602+aabbddss1@users.noreply.github.com>
2021-08-30 02:56:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,107 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
+import ODataStore from 'devextreme/data/odata/store';
 
-    </div>
-  );
+import DataGrid, {
+  Column,
+  Summary,
+  TotalItem,
+  ValueFormat,
+  GroupItem,
+  Grouping,
+  GroupPanel,
+  Pager,
+  Paging,
+  SearchPanel
+} from 'devextreme-react/data-grid';
+
+import DiscountCell from './DiscountCell.js';
+
+const pageSizes = [10, 25, 50, 100];
+
+const dataSourceOptions = {
+  store: new ODataStore({
+    url: "./mock_data.json",
+    key: 'value',
+   
+  })
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    };
+    this.onContentReady = this.onContentReady.bind(this);
+  }
+  render() {
+    return (
+      <DataGrid
+        dataSource={dataSourceOptions}
+        allowColumnReordering={true}
+        showBorders={true}
+        onContentReady={this.onContentReady}
+      >
+        <GroupPanel visible={true} />
+        <SearchPanel visible={true} highlightCaseSensitive={true} />
+        <Grouping autoExpandAll={false} />
+
+        <Column dataField="Sales Data" groupIndex={0} />
+      
+        
+        <Column dataField="name" dataType="string" />
+        <Column dataField="company" dataType="string" width={150} />
+        <Column
+          dataField="sales"
+          caption="Sales"
+          dataType="number"
+          format="currency"
+          alignment="right"
+        />
+        <Column
+          dataField="sales"
+          caption="Graphic"
+          dataType="number"
+          format="percent"
+          alignment="right"
+          allowGrouping={false}
+          cellRender={DiscountCell}
+          cssClass="bullet"
+          
+        /> 
+        
+        <Summary>
+            <TotalItem
+              column="sales"
+              summaryType="count" />
+            <TotalItem
+              column="sales"
+              summaryType="min"
+              customizeText={this.customizeDate} />
+              <TotalItem
+              column="sales"
+              summaryType="max"
+              customizeText={this.customizeDate} />
+            <TotalItem
+              column="sales"
+              summaryType="sum"
+              valueFormat="currency" />
+          </Summary>
+        <Pager allowedPageSizes={pageSizes} showPageSizeSelector={true} />
+        <Paging defaultPageSize={10} />
+      </DataGrid>
+    );
+  }
+
+  onContentReady(e) {
+    if (!this.state.collapsed) {
+      e.component.expandRow(['EnviroCare']);
+      this.setState({
+        collapsed: true
+      });
+    }
+  }
 }
 
 export default App;
